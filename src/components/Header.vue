4afb9ccfab4c2796c3027fd88b7ace67b1491ec3
BaccanoB <beatrice.baccano@yahoo.it>
2021-06-09 14:47:27 +0200

    header completato

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <img src="../assets/spotify.png" alt="spotify_logo">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../style/mixins';
+    div {
+        @include flex;
+        width: 100%;
+        height: 50px;
+        background-color: #2E3A46;
+
+        img {
+            width: 40px;
+            margin-left: 10px;
+        }
+    }
+</style>
